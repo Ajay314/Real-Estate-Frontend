f98cd4f7496d29d0f553d58ae25df67a55bcd993
Ajay314 <109684456+Ajay314@users.noreply.github.com>
2024-10-25 23:19:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Signup from './Signup.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Login.js';
+import AddProperty from './AddProperty.js';
+import AdminSignup from './AdminSignUp.js';
+import AdminLogin from './AdminLogin.js';
+import PropertyList from './GetProperty.js';
+import BookingPage from './Booking.js';
+
+
+function App() {
+  return (
+  //   <BrowserRouter>
+
+  //   <Routes>
+  //      <Route path='/signup' element={<Signup />}></Route>
+  //      <Route path='/login' element={<Login />}></Route>
+      
+  //   </Routes>
+  // </BrowserRouter>
+  <div>
+    {/* <Signup />
+        
+      
+    {/* <AddProperty /> */}
+    {/* <AdminSignup /> */}
+    {/* <AdminLogin /> */}
+    {/* <PropertyList /> */}
+    {/* <Login /> */}
+    {/* <BookingPage /> */}
+  </div>
+  
+)
+
+}
+
+export default App;
